3623c657a78964e820276b50bdb21e4e5726ab27
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-07-13 10:57:15 +0530

    add new feature

--- codemancers/src/component/Gif.jsx
+++ codemancers/src/component/Gif.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import "./style/Gifs.css";
 
-export const Gif = () => {
+export const Gif = ({handle}) => {
   const GIPHY_API = `https://api.giphy.com/v1/gifs/search?api_key=yCH88qDr3JCOEEWuESg34aXKYSbyUVQc&limit=25&offset=0&q=`;
   let [search, setSearch] = useState("");
   let [gifs, setGifs] = useState([]);
   let [loading, setLoading] = useState(false);
-
+  const handlegif=(e)=>{
+      console.log(e)
+      handle(e)
+  }
   const searchGif = () => {
 
     if (search.length > 0) {
@@ -43,7 +46,7 @@ export const Gif = () => {
       {gifs.map((g,i) => {
         return (
           <div className='f'key={i}>
-            <img className='i' src={g} alt="" />
+            <img className='i' src={g} alt="" onClick={(e)=>handlegif(e.target.src)} />
           </div>
         );
       })}
--- codemancers/src/component/Post.jsx
+++ codemancers/src/component/Post.jsx
@@ -1,7 +1,7 @@
 import "./style/Post.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import GifBoxIcon from '@mui/icons-material/GifBox';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
@@ -14,7 +14,13 @@ import { useState } from "react";
 import { Gif } from '../component/Gif';
 
 export const Post = () => {
-  const [state,setState] = useState(0)
+  const [state,setState] = useState(0);
+  const [gif,setGifs] = useState("")
+  const handle=(value)=>{
+    console.log(value)
+    setState(0)
+    setGifs(value)
+  }
   return (
     <div className="Post">
       <div className="navbar">
@@ -23,38 +29,38 @@ export const Post = () => {
         <p> <VideoCallIcon/> Live Video</p>
       </div>
       <TextField
-        sx={{ border: 1, width: 500 }}
+        sx={{width: 500 }}
         multiline
-        rows={5}
+        rows={2}
         variant="standard"
         placeholder= "Write Something here..."
         />
-        
-      <img src="" alt="" />
+      <img src={gif} alt="" />
+      
       <div className="but">
-      <Button variant="contained" disableElevation>
+      <Button variant="contained" disableElevation sx={{fontSize:10}}>
       <PersonAddIcon/> Tag friends
       </Button>
-      <Button variant="contained" disableElevation>
+      <Button variant="contained" disableElevation sx={{fontSize:10}}>
        <LocationOnIcon/>check in
       </Button>
-      <Button variant="contained" disableElevation onClick={()=>{setState(1)}}>
+      <Button variant="contained" disableElevation onClick={()=>{setState(1)}} sx={{fontSize:10}}>
        <GifBoxIcon/> gif
       </Button>
-      <Button variant="contained" disableElevation>
+      <Button variant="contained" disableElevation sx={{fontSize:10}}>
       <CalendarMonthIcon/> Tag event
       </Button>
       </div>
       <div className="footer">
-      <Button variant="contained" disableElevation sx={{}}>
-      <LockIcon/> Only me
+      <Button variant="contained" disableElevation sx={{fontSize:10}}>
+      <LockIcon fontSize="x-small"/> Only me
       </Button>
-      <Button variant="contained" disableElevation>
+      <Button variant="contained" disableElevation sx={{fontSize:10}}>
        Post
       </Button>
       </div>
       {
-        state?<Gif/>:""
+        state?<Gif handle={handle}/>:""
       }
     </div>
   );
